1fc162b63796127867c310e75454c714f1354be9
mityaycho <mityaycho@gmail.com>
2023-10-22 22:32:07 +0300

    add Input event

--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "./Input";
 import { Button } from "./Button";
 import { subscribe } from "diagnostics_channel";
 
@@ -16,16 +17,33 @@ export type PropsType = {
 
 export function Todolist(props: PropsType) {
 
-	
+	let [message, setMessage] = useState([
+		{message: "message1"},
+		{message: "message2"},
+		{message: "message3"}
+	])
+
+	let [title, setTitle] = useState('');
+
+	const addMessage = (title: string) => {
+		let newMessage = {message: title};
+		setMessage([newMessage, ...message])
+	}
 	const inputs = props.tasks.map((el) => <li key={el.id}><input type="checkbox" checked={el.isDone} /> <span>{el.title}</span><Button name="x" callBack={() => props.delTasks(el.id)}/></li>);
+
+	const callBackButtonHandler = () => {
+		addMessage(title);
+		setTitle("");
+	}
 
 
 	return (
 		<div>
 			<h3>{props.title}</h3>
 			<div>
-				<input />
-				<button>+</button>
+				<Input setTitle={setTitle} title={title} />
+				<Button name = {"+"} callBack={callBackButtonHandler}/>
+				{message.map((el, i) => <div key={i}>{el.message}</div>)}
 			</div>
 			<ul>
 				{inputs}
