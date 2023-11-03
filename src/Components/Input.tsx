d0ff772847547b76afa453d6888f44afde9a3d0e
mityaycho <mityaycho@gmail.com>
2023-11-03 21:03:38 +0300

    add some changes

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, KeyboardEvent } from "react";
+import React, { KeyboardEvent } from "react";
 
 type InputPropsType = {
 	title: string;
@@ -10,10 +10,6 @@ type InputPropsType = {
 
 export function Input(props: InputPropsType) {
 
-	const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-		props.setTitle(event.currentTarget.value);
-	};
-
 	const onKeyDownEventHandler = (event: KeyboardEvent<HTMLInputElement>) => {
 		props.setError();
 		event.key === "Enter" && props.addTask();
@@ -22,7 +18,6 @@ export function Input(props: InputPropsType) {
 	return <input 
 	className={props.error ? "error" : ""} 
 	value={props.title} 
-	onChange={onChangeInputHandler} 
 	onKeyDown={onKeyDownEventHandler} 
 	/>;
 };
--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -1,4 +1,4 @@
-import React, { useState, KeyboardEvent, ChangeEvent } from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Input } from "./Input";
 import { Button } from "./Button";
 
@@ -35,7 +35,11 @@ export function Todolist(props: PropsType) {
 		setMessage([newMessage, ...message]);
 	};
 
-	const inputs = props.tasks.map((el) => <li key={el.id} className={el.isDone ? "is-done" : ""}><input type="checkbox" checked={el.isDone} onChange={() => props.changeTaskStatus(el.id, el.isDone)}/> <span>{el.title}</span><Button classNameStatus="" name="x" callBack={() => props.delTasks(el.id)}/></li>);
+	const onChangeHandler = (id: string, event: ChangeEvent<HTMLInputElement>) => {
+		props.changeTaskStatus(id, event.currentTarget.checked);
+	};
+
+	const inputs = props.tasks.map((el) => <li key={el.id} className={el.isDone ? "is-done" : ""}><input type="checkbox" checked={el.isDone} onChange={(e) => onChangeHandler(el.id, e)}/> <span>{el.title}</span><Button classNameStatus="" name="x" callBack={() => props.delTasks(el.id)}/></li>);
 
 	const addTask = () => {
 		props.addTask(title);
