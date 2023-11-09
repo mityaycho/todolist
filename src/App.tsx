c5f16fcd1c0d3373d08faa474a6c12051cef9fa5
mityaycho <mityaycho@gmail.com>
2023-11-09 22:20:48 +0300

    add some changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import { v1 } from 'uuid';
 function App() {
 
 	const [tasks, setTasks] = useState ([
-		{ id: v1(), title: "CSS", isDone: true },
+		{ id: v1(), title: "HTML&CSS", isDone: true },
 		{ id: v1(), title: "JS", isDone: true },
-		{ id: v1(), title: "React", isDone: false }
+		{ id: v1(), title: "ReactJS", isDone: false },
+		{ id: v1(), title: "Rest API", isDone: false },
+		{ id: v1(), title: "GraphQL", isDone: false }
 	]);
 
 	const [error, setError] = useState<string | null | undefined>(null);
