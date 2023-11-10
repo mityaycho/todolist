50d346c5f0b40ccd4b3b713fd2b6aa3edff51fc9
mityaycho <mityaycho@gmail.com>
2023-11-10 22:27:50 +0300

    add associative arrays

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,35 @@ import { v1 } from 'uuid';
 
 function App() {
 
-	const [tasks, setTasks] = useState([
-		{ id: v1(), title: "HTML&CSS", isDone: true },
-		{ id: v1(), title: "JS", isDone: true },
-		{ id: v1(), title: "ReactJS", isDone: false },
-		{ id: v1(), title: "Rest API", isDone: false },
-		{ id: v1(), title: "GraphQL", isDone: false }
-	]);
+	// const [tasks, setTasks] = useState([
+	// 	{ id: v1(), title: "HTML&CSS", isDone: true },
+	// 	{ id: v1(), title: "JS", isDone: true },
+	// 	{ id: v1(), title: "ReactJS", isDone: false },
+	// 	{ id: v1(), title: "Rest API", isDone: false },
+	// 	{ id: v1(), title: "GraphQL", isDone: false }
+	// ]);
+
+	let todolistID1 = v1()
+	let todolistID2 = v1()
+
+	let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+		{ id: todolistID1, title: 'What to learn', filter: 'all' },
+		{ id: todolistID2, title: 'What to buy', filter: 'all' },
+	])
+
+	let [tasks, setTasks] = useState({
+		[todolistID1]: [
+			{ id: v1(), title: 'HTML&CSS', isDone: true },
+			{ id: v1(), title: 'JS', isDone: true },
+			{ id: v1(), title: 'ReactJS', isDone: false },
+
+		],
+		[todolistID2]: [
+			{ id: v1(), title: 'Rest API', isDone: true },
+			{ id: v1(), title: 'GraphQL', isDone: false },
+		]
+	})
+
 
 	const [error, setError] = useState<string | null | undefined>(null);
 
