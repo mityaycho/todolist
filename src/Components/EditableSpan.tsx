c2559934fb2be4523ac65e6b7a660e8d858c2dd8
mityaycho <mityaycho@gmail.com>
2023-11-21 20:54:51 +0300

    add onChangeTitleHandler to Editable component

--- src/Components/EditableSpan.tsx
+++ src/Components/EditableSpan.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 
 type EditableSpanPropsType = {
@@ -7,13 +7,18 @@ type EditableSpanPropsType = {
 
 const EditableSpan = (props: EditableSpanPropsType) => {
 	const [editMod, setEditMode] = useState(false);
+	const [title, setTitle] = useState("");
 
-	const activateEditMode = () => setEditMode(true);
+	const activateEditMode = () => {
+		setEditMode(true);
+		setTitle(props.title);
+	};
 	const activateViewMode = () => setEditMode(false);
+	const onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
 
 	return (
 		editMod ?
-		<input value={props.title} onBlur={activateViewMode} autoFocus /> :
+		<input value={title} onChange={onChangeTitleHandler} onBlur={activateViewMode} autoFocus /> :
 		<span onDoubleClick={activateEditMode}>{props.title}</span>
 
 	);
