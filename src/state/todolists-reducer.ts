bc594078882ca43b279c7d1f58f28386c1c4ec79
mityaycho <mityaycho@gmail.com>
2023-12-02 18:55:02 +0300

    add reducer 'CHANGE-TODOLIST-FILTER'

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -18,8 +18,11 @@ export const todolistsReducer = (state: Array<TodolistType>, action: ActionType)
 			const todolistId = v1();
 			return [...state, { id: todolistId, title: action.title, filter: "all" }];
 		case 'CHANGE-TODOLIST-TITLE':
-			const newTitle = state.map(el => el.id === action.id ? {...el, title: action.title} : el);
+			const newTitle = state.map(el => el.id === action.id ? { ...el, title: action.title } : el);
 			return [...newTitle];
+		case 'CHANGE-TODOLIST-FILTER':
+			const newFilter = state.map(el => el.id === action.id && el.filter !== action.filter ? { ...el, filter: action.filter } : el);
+			return [...newFilter];
 		default:
 			throw new Error('I don\'t understand this type');
 	};
