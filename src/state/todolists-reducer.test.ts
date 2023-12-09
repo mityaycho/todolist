d41fd5aa05762210dc20a18810fab82e537ee473
mityaycho <mityaycho@gmail.com>
2023-12-09 23:29:18 +0300

    add some changes

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -11,7 +11,7 @@ test('correct todolist should be removed', () => {
         {id: todolistId2, title: 'What to buy', filter: 'all'}
     ];
 
-    const endState = todolistsReducer(startState, RemoveTodolistAC(todolistId1))
+    const endState = todolistsReducer(startState, RemoveTodolistAC(todolistId1));
 
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todolistId2)
