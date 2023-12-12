70862836f094b92fa991d25af53c2a7086fd527a
mityaycho <mityaycho@gmail.com>
2023-12-12 23:55:34 +0300

    add some changes

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -16,7 +16,7 @@ test('correct todolist should be removed', () => {
     const endState = todolistsReducer(startState, RemoveTodolistAC(todolistId1));
 
     expect(endState.length).toBe(1);
-    expect(endState[0].id).toBe(todolistId2)
+    expect(endState[0].id).toBe(todolistId2);
 })
 
 ​test('correct todolist should be added', () => {
