e20f884389b0ff4b82c4c7897c340783108a3eba
mityaycho <mityaycho@gmail.com>
2023-12-13 23:19:10 +0300

    add return to case

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -51,7 +51,7 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType): TasksS
 			const changeTitle = state[action.todolistId].map(el => el.id === action.taskId && el.title !== action.title ? { ...el, title: action.title } : el);
 			return { ...state, [action.todolistId]: changeTitle };
 		case 'ADD-TODOLIST':
-			return {}
+			return { ...state, [v1()]: [] }
 		default:
 			throw new Error('I don\'t understand this type');
 	};
