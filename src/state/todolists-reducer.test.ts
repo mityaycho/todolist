b598fbebcd625ce2aa676b7eac0acfc5e2ea4466
mityaycho <mityaycho@gmail.com>
2023-12-22 14:03:12 +0300

    delete variable in tests

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -17,7 +17,6 @@ beforeEach(() => {
 
 
 test('correct todolist should be removed', () => {
-
 	const endState = todolistsReducer(startState, RemoveTodolistAC(todolistId1));
 
 	expect(endState.length).toBe(1);
@@ -25,15 +24,7 @@ test('correct todolist should be removed', () => {
 });
 
 test('correct todolist should be added', () => {
-	let todolistId1 = v1()
-	let todolistId2 = v1()
-
 	let newTodolistTitle = 'New Todolist';
-
-	const startState: Array<TodolistType> = [
-		{ id: todolistId1, title: 'What to learn', filter: 'all' },
-		{ id: todolistId2, title: 'What to buy', filter: 'all' }
-	]
 
 	const endState = todolistsReducer(startState, addTodolistAC(newTodolistTitle))
 
@@ -42,15 +33,7 @@ test('correct todolist should be added', () => {
 })
 
 test('correct todolist should change its name', () => {
-	let todolistId1 = v1()
-	let todolistId2 = v1()
-
 	let newTodolistTitle = 'New Todolist'
-
-	const startState: Array<TodolistType> = [
-		{ id: todolistId1, title: 'What to learn', filter: 'all' },
-		{ id: todolistId2, title: 'What to buy', filter: 'all' }
-	]
 
 	const action = {
 		type: 'CHANGE-TODOLIST-TITLE' as const,
@@ -65,15 +48,7 @@ test('correct todolist should change its name', () => {
 })
 
 test('correct filter of todolist should be changed', () => {
-	let todolistId1 = v1()
-	let todolistId2 = v1()
-
 	let newFilter: string = 'completed'
-
-	const startState: Array<TodolistType> = [
-		{ id: todolistId1, title: 'What to learn', filter: 'all' },
-		{ id: todolistId2, title: 'What to buy', filter: 'all' }
-	]
 
 	const action = {
 		type: 'CHANGE-TODOLIST-FILTER' as const,
