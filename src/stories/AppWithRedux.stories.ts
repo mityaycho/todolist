35c867f27520c2780596234d6f643ae3d3a87ad1
mityaycho <mityaycho@gmail.com>
2024-01-31 21:41:07 +0300

    add AppWithRedux.stories

--- src/stories/AppWithRedux.stories.ts
+++ src/stories/AppWithRedux.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import AppWithRedux from "../AppWithRedux";
+import { ReduxStoreProviderDecorator } from './decorators/ReduxStoreProviderDecorator';
 
 // More on how to set up stories at:
 // https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -10,7 +11,7 @@ const meta: Meta<typeof AppWithRedux> = {
 	// https://storybook.js.org/docs/react/writing-docs/autodocs
 	tags: ['autodocs'],
 	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-
+	decorators: [ReduxStoreProviderDecorator]
 };
 
 export default meta;
