fdf4a57999b230b19e69b593ed34701d75f0a04e
mityaycho <mityaycho@gmail.com>
2024-02-21 22:26:19 +0300

    add some changes

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ import {
 import { addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC } from './state/tasks-reducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppRootStateType } from './state/store';
-import { TaskStatuses, TaskType } from './api/todolists-api'
+import { TaskStatuses, TaskType } from './api/todolists-api';
 
 
 export type TasksStateType = {
